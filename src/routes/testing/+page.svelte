24424f52fa0b7b45c209d9e1db7e97d0896a5824
spencer <spenceracbyrne@gmail.com>
2025-01-09 02:30:01 -0600

    new fav <3

--- src/routes/testing/+page.svelte
+++ src/routes/testing/+page.svelte
@@ -4,17 +4,20 @@
 	import { cn } from '$lib/utils';
 	import { Extension } from '@tiptap/core';
 	import type { KeyboardShortcutCommand } from '@tiptap/core';
-	import { Button } from "$lib/components/ui/button";
+	import { Button } from '$lib/components/ui/button';
 
 	import '@friendofsvelte/tipex/styles/Tipex.css';
 	import '@friendofsvelte/tipex/styles/ProseMirror.css';
 
 	import '@friendofsvelte/tipex/styles/CodeBlock.css';
 	import Control from '$lib/components/features/messages/Control.svelte';
-    import RichMessageComposer from '$lib/components/features/messages/RichMessageComposer.svelte';
-
+	import RichMessageComposer from '$lib/components/features/messages/RichMessageComposer.svelte';
 </script>
 
-<div class="fixed bottom-0 left-0 right-0">
-  <RichMessageComposer />
+<div class="bg-white h-full">
+	<div class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[80vh] h-[80vh] bg-background rounded-3xl flex items-center justify-center pt-10">
+		<div class="font-chattie text-[75vh] bg-gradient-to-r from-blue-400/60 to-purple-600/60 bg-clip-text text-transparent">
+			C
+		</div>
+	</div>
 </div>
